d0e5fa4e8155c920139edf008d8b9d640278cdbb
konraafk <izat02begimbaev@gmail.com>
2023-03-07 00:16:14 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,121 @@
+// let arrTwo = [
+// 'petya',
+//     {
+//     type:"JS",
+//     age:36,
+// },
+// true,
+// function(){
+// console.log('Privet, yua petya');
+//     }
+// ];
+// console.log(arrTwo);
+//  console.log(arrTwo[0]);
+//  console.log(arrTwo[1]);
+//  console.log(arrTwo[2]);
+//  arrTwo[3]();
+
+
+// Многомерные массивы
+ 
+// let arrOne = [
+//     'Ваня',
+//     'Иштнван',
+//     'Оля',
+// ];
+// console.log(arrOne[1]);
+// console.log(arrOne[2]);
+
+
+
+// let arr = ['Ваня','Иштнван','Оля',];
+// console.log(arr);
+
+// let arrNeew = arr;
+
+// arrNew.length  < 2;
+
+// console.log(arrNew);
+
+// let arr = ['Ваня','Иштнван','Оля',];
+
+// arr[0] = 'Kolyua';
+// console.log(arr);
+
+// arr[3] = 'Ваня';
+// console.log(arr);
+
+//  let arr = ['Ваня','Иштнван','Оля',];
+//  arr.push('Ваня');
+//  console.log(arr);
+
+//  arr.push('Dima','Katya',)
+//  console.log(arr);
+
+
+// let arr = ['Ваня','Иштнван','Оля',];
+// arr.shift()
+// console.log(arr);
+
+// let arr = ['Ваня','Иштнван','Оля',];
+// arr.pop();
+// console.log(arr);
+
+
+// let arr = ['Ваня','Иштнван','Оля',];
+// arr.unshift('Ваcя');
+// console.log(arr);
+
+// arr.unshift('Dima','Katya',);
+// console.log(arr);
+
+// let arr = ['Ваня','Иштнван','Оля',];
+// delete arr[1];
+// console.log(arr);
+// console.log(arr[1]);
+// console.log(arr.length);
+
+// let arr = ['Ваня','Иштнван','Оля',];
+// arr.splice(1, 1);
+// console.log(arr);
+
+
+// let arr = ['Ваня','Иштнван','Оля',];
+// let removed = arr.splice(1);
+// console.log(removed);
+
+// let arr = ['Ваня','Иштнван','Оля',];
+// arr.splice(0, 1, 'Dima');
+// console.log(arr);
+
+// let arr = ['Ваня','Иштнван','Оля',];
+// arr.splice(0, 0,'Dima','Katya',); 
+// console.log(arr);
+
+
+// let arr = ['Ваня','Иштнван','Оля',];
+// arr.splice(-1, 1);
+// console.log(arr);
+
+
+// let arrOne = ['Ваня','Иштнван','Оля',];
+// arrTwo = arrOne.slice(1, 2)
+// console.log(arrTwo);
+
+
+// const task1 = (list) => {
+//     return document.querySelector('li').textContent
+// }
+// console.log(task1());
+
+//  const task2 = []
+//  const newTask = () => {
+//     return Array.from(document.getElementsByTagName('li')).map(el => el.textContent)
+//     }
+//     console.log(newTask());
+
+// const task3 = () => {
+//     return Array.from(document.getElementsByTagName('li class')).textContent
+// }
+// console.log(task3());
+ 
